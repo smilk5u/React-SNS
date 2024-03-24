58a606cda1e8173a12118c153c681d58d410bedb
amilk5u <amilk5u@naver.com>
2024-03-25 01:57:29 +0900

    first commit

---
+++ client/components/MagItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const MagItem = ({
+  id,
+  userId,
+  timestamp,
+  text
+}) => {
+
+  return (
+    <li className="messages__item">
+      <h3>
+        {userId} {''}
+        <sub>
+          {
+            new Date(timestamp).toLocaleString('ko-KR', {
+              year: 'numeric',
+              month: 'numeric',
+              day: 'numeric',
+              hour: '2-digit',
+              minute: '2-digit',
+              hour12: true,
+            })
+          }
+        </sub>
+      </h3>
+      {text}
+    </li>
+  );
+};
+
+export default MagItem;
---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import MagList from "../components/MagList";
+
+const index = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <MagList />
+    </>
+  );
+};
+
+export default index;
